7bbe2b9450ca5280c2bc9c2a0ac248b9a7757d48
lajpie <78029332+lajpie@users.noreply.github.com>
2024-11-29 14:17:07 -0500

    pointeVers

--- robot.cpp
+++ robot.cpp
@@ -9,10 +9,10 @@ class robot
 private:
     /* data */
     char direction;
-    
 
 public:
-    robot(char dirInit){
+    robot(char dirInit)
+    {
         switch (dirInit)
         {
         case 'N':
@@ -22,12 +22,12 @@ public:
         case 'S':
         case 's':
         case 'E':
-        case 'e':        
-        direction = dirInit;
-        break;
+        case 'e':
+            direction = dirInit;
+            break;
 
         default:
-        cout << "Direction non reconnue, le robot pointe vers le nord par défaut" << endl;
+            cout << "Direction non reconnue, le robot pointe vers le nord par défaut" << endl;
             direction = 'N';
             break;
         }
@@ -47,7 +47,7 @@ public:
     void roterG()
     {
         cout << "roter 90 degrées gauche" << endl;
-        
+
         switch (direction)
         {
         case 'N':
@@ -75,11 +75,11 @@ public:
         }
     }
 
-    void RoterD()
+    void roterD()
     {
         cout << "roter 90 degrées droite" << endl;
 
-         switch (direction)
+        switch (direction)
         {
         case 'N':
         case 'n':
@@ -99,6 +99,116 @@ public:
         case 'E':
         case 'e':
             direction = 'S';
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    void pointeVers(char versDir)
+    {
+        //vérifie ou le robot veux aller et tourne selon ou le robot pointe déjà
+        switch (versDir)
+        {
+        case 'N':
+        case 'n':
+            switch (direction)
+            {
+            case 'O':
+            case 'o':
+                roterD();
+                break;
+
+            case 'S':
+            case 's':
+                roterD();
+                roterD();
+                break;
+
+            case 'E':
+            case 'e':
+                roterG();
+                break;
+
+            default:
+                break;
+            }
+            break;
+
+        case 'O':
+        case 'o':
+            switch (direction)
+            {
+            case 'N':
+            case 'n':
+                roterG();
+                break;
+
+            case 'S':
+            case 's':
+                roterD();
+                break;
+
+            case 'E':
+            case 'e':
+                roterG();
+                roterG();
+                break;
+
+            default:
+                break;
+            }
+            break;
+
+        case 'S':
+        case 's':
+            switch (direction)
+            {
+            case 'N':
+            case 'n':
+                roterG();
+                roterG();
+                break;
+
+            case 'O':
+            case 'o':
+                roterG();
+                break;
+
+            case 'E':
+            case 'e':
+                roterD();
+                break;
+
+            default:
+                break;
+            }
+            break;
+
+        case 'E':
+        case 'e':
+            switch (direction)
+            {
+            case 'N':
+            case 'n':
+                roterD();
+                break;
+
+            case 'O':
+            case 'o':
+                roterG();
+                roterG();
+                break;
+
+            case 'S':
+            case 's':
+                roterG();
+                break;
+
+            default:
+                break;
+            }
             break;
 
         default:

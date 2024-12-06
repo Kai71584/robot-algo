1584f267426d449d795dfb00bc0be368ec4bd6e9
lajpie <78029332+lajpie@users.noreply.github.com>
2024-12-06 14:39:26 -0500

    commentaires

--- robot.cpp
+++ robot.cpp
@@ -1,9 +1,17 @@
+/*
+fichier: robot.cpp
+autheur: Pierre-Antoine Lajoie
+dernière MAJ: 2024-12-06 14:30
+description: fonctions de la classe Robot
+*/
+
 #include "robot.h"
 #include <unordered_map>
 #include <unordered_set>
 using namespace std;
 ;
 
+// constructeur
 Robot::Robot(string dirInit)
 {
     if (dirInit == "NN" || dirInit == "OO" || dirInit == "SS" || dirInit == "EE")
@@ -16,60 +24,71 @@ Robot::Robot(string dirInit)
     }
 }
 
-Robot::Robot() {
+// constructeur par défaut
+Robot::Robot()
+{
     direction = "EE";
 };
 
+// fait avancer le robot
 void Robot::avancer()
 {
     cout << "avance de 10cm" << endl;
 }
 
+// fait reculer le robot
 void Robot::reculer()
-    {
-        cout << "recule de 10cm" << endl;
-    }
+{
+    cout << "recule de 10cm" << endl;
+}
 
+// fait tourner le robot à gauche
 void Robot::roterG()
+{
+    cout << "roter 90 degrées gauche" << endl;
+
+    if (direction == "NN")
     {
-        cout << "roter 90 degrées gauche" << endl;
-
-        if (direction=="NN")
-        {
-            direction = "OO";
-        } else if (direction=="OO")
-        {
-            direction = "SS";
-        }else if (direction=="SS")
-        {
-            direction = "EE";
-        }else if (direction=="EE")
-        {
-            direction = "NN";
-        }
-        
-        
+        direction = "OO";
     }
+    else if (direction == "OO")
+    {
+        direction = "SS";
+    }
+    else if (direction == "SS")
+    {
+        direction = "EE";
+    }
+    else if (direction == "EE")
+    {
+        direction = "NN";
+    }
+}
 
+// fait tourner le robot à droite
 void Robot::roterD()
+{
+    cout << "roter 90 degrées droite" << endl;
+
+    if (direction == "NN")
     {
-        cout << "roter 90 degrées droite" << endl;
-
-        if (direction=="NN")
-        {
-            direction = "EE";
-        } else if (direction=="EE")
-        {
-            direction = "SS";
-        }else if (direction=="SS")
-        {
-            direction = "OO";
-        }else if (direction=="OO")
-        {
-            direction = "NN";
-        }
+        direction = "EE";
     }
+    else if (direction == "EE")
+    {
+        direction = "SS";
+    }
+    else if (direction == "SS")
+    {
+        direction = "OO";
+    }
+    else if (direction == "OO")
+    {
+        direction = "NN";
+    }
+}
 
+// affiche la direction dans laquelle le robot
 void Robot::afficheDir()
 {
     cout << "je pointe vers " << direction << endl;
--- robot.h
+++ robot.h
@@ -1,3 +1,11 @@
+/*
+fichier: robot.h
+autheur: Pierre-Antoine Lajoie
+dernière MAJ: 2024-12-06 14:30
+description: déclarations de la classe Robot
+*/
+
+
 #ifndef __ROBOT_H__
 #define __ROBOT_H__
 #include <iostream>
@@ -10,17 +18,23 @@ private:
     string direction;
 
 public:
+    // constructeurs
     Robot(string dirInit);
     Robot();
 
+    // affiche la direction dans laquelle le robot
     void afficheDir();
 
+    // fait avancer le robot
     void avancer();
 
+    // fait reculer le robot
     void reculer();
 
+    // fait tourner le robot à gauche
     void roterG();
 
+    // fait tourner le robot à droite
     void roterD();
 };
 
